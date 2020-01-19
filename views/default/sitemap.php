997a304333a0896ad151e62d8e2115c3a0bf28b8
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-01-20 00:41:08 +0200

    Html encode for url`s in xml

--- views/default/sitemap.php
+++ views/default/sitemap.php
@@ -1,15 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php foreach ($items as $item): ?>
 <?php if (isset($item['url']) && isset($item['updated_at'])) : ?>
     <url>
-        <loc><?php echo $item['url']; ?></loc>
-        <lastmod><?php echo $item['updated_at']; ?></lastmod>
+        <loc><?= Html::encode($item['url']); ?></loc>
+        <lastmod><?= $item['updated_at']; ?></lastmod>
 <?php if (isset($item['changefreq'])) : ?>
-        <changefreq><?php echo $item['changefreq']; ?></changefreq>
+        <changefreq><?= $item['changefreq']; ?></changefreq>
 <?php endif; ?>
 <?php if (isset($item['priority'])) : ?>
-        <priority><?php echo $item['priority']; ?></priority>
+        <priority><?= $item['priority']; ?></priority>
 <?php endif; ?>
     </url>
 <?php endif; ?><?php endforeach; ?>
